49af09e39ab19d65310049c423c26b2e65a27911
Nicholas Whittaker <nicholas@nicholas.cloud>
2023-03-26 14:07:46 +1100

    Fix typo, group logs in Buildkite

--- .buildkite/plausible-backup/backup.sh
+++ .buildkite/plausible-backup/backup.sh
@@ -2,6 +2,9 @@
 
 set -euo pipefail
 
+echo "--- Preparing runtime environment"
 python3 -m venv .venv
 make -C playbooks install
+
+echo "--- Backing up Plausible instance"
 make -C playbooks backup-plausible
--- .buildkite/plausible-backup/upload.sh
+++ .buildkite/plausible-backup/upload.sh
@@ -2,7 +2,8 @@
 
 set -euo pipefail
 
-AWS_CREDENTIALS="$(vault write -format=json aws/sts/Terraform)"
+echo "--- Retrieving AWS credentials"
+AWS_CREDENTIALS="$(vault read -format=json aws/sts/Terraform)"
 AWS_ACCESS_KEY_ID="$(echo "$AWS_CREDENTIALS" | jq --raw-output .access_key)"
 export AWS_ACCESS_KEY_ID
 AWS_SECRET_ACCESS_KEY="$(echo "$AWS_CREDENTIALS" | jq --raw-output .secret_key)"
@@ -10,6 +11,8 @@ export AWS_SECRET_ACCESS_KEY
 AWS_SESSION_TOKEN="$(echo "$AWS_CREDENTIALS" | jq --raw-output .session_token)"
 export AWS_SESSION_TOKEN
 
+echo "--- Downloading backup artifact"
 buildkite-agent artifact download "playbooks/backups/**/*.tar" .
 
+echo "--- Uploading backup to S3"
 aws s3 cp --recursive playbooks/backups/gandra-dee/mnt/backups/analytics/ s3://nchlswhttkr/plausible/
