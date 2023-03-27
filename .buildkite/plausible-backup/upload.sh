94b1e5a173000dbccef31395b17d1034cbaa6d6c
Nicholas Whittaker <nicholas@nicholas.cloud>
2023-03-27 17:10:36 +1100

    Remove unnecessary sleep

--- .buildkite/plausible-backup/upload.sh
+++ .buildkite/plausible-backup/upload.sh
@@ -6,5 +6,4 @@ echo "--- Downloading backup artifact"
 buildkite-agent artifact download "playbooks/backups/**/*.tar" .
 
 echo "--- Uploading backup to S3"
-sleep 30 # Pretty sure STS should not 
 aws s3 cp --recursive playbooks/backups/gandra-dee/mnt/backups/analytics/ s3://nchlswhttkr/plausible/
