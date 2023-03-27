59bc59e9274cfe2e7e2d591b4a3b06fdae5c316c
Nicholas Whittaker <nicholas@nicholas.cloud>
2023-03-27 17:07:30 +1100

    Assume AWS role in CI via environment variable

--- .buildkite/plausible-backup/upload.sh
+++ .buildkite/plausible-backup/upload.sh
@@ -2,15 +2,6 @@
 
 set -euo pipefail
 
-echo "--- Retrieving AWS credentials"
-AWS_CREDENTIALS="$(vault read -format=json aws/sts/Terraform)"
-AWS_ACCESS_KEY_ID="$(echo "$AWS_CREDENTIALS" | jq --raw-output .data.access_key)"
-export AWS_ACCESS_KEY_ID
-AWS_SECRET_ACCESS_KEY="$(echo "$AWS_CREDENTIALS" | jq --raw-output .data.secret_key)"
-export AWS_SECRET_ACCESS_KEY
-AWS_SESSION_TOKEN="$(echo "$AWS_CREDENTIALS" | jq --raw-output .data.security_token)"
-export AWS_SESSION_TOKEN
-
 echo "--- Downloading backup artifact"
 buildkite-agent artifact download "playbooks/backups/**/*.tar" .
 
