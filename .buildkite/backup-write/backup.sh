194d43ebc6330adb8425fc0fb17e283f3e9c1279
Nicholas Whittaker <nicholas@nicholas.cloud>
2025-06-21 22:45:07 +1000

    Fix permissions error from backup (#6)

--- .buildkite/backup-write/backup.sh
+++ .buildkite/backup-write/backup.sh
@@ -7,3 +7,8 @@ make -C deploy
 
 echo "--- Backing up Writefreely instance"
 make -C deploy/writefreely backup
+
+echo "--- Uploading backups as build artifacts"
+# Ansible syncing files breaks subsequent builds, upload directly and purge now
+buildkite-agent artifact upload "deploy/writefreely/backups/*.tar"
+rm -r deploy/writefreely/backups
