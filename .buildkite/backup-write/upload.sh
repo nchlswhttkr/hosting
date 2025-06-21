5545c01f99a0784c80a271afde4dd1c4d4ef6415
Nicholas Whittaker <nicholas@nicholas.cloud>
2025-06-21 21:55:30 +1000

    Fix Writefreely backup paths

--- .buildkite/backup-write/upload.sh
+++ .buildkite/backup-write/upload.sh
@@ -21,9 +21,9 @@ AWS_SESSION_TOKEN="$(echo "$AWS_CREDENTIALS" | jq --raw-output .Credentials.Sess
 export AWS_SESSION_TOKEN
 
 echo "--- Downloading backup artifact"
-buildkite-agent artifact download "deploy/boyd/writefreely/backups/**/*.tar" .
+buildkite-agent artifact download "deploy/writefreely/backups/*.tar" .
 
 echo "--- Uploading backup to S3"
 export AWS_REGION="ap-southeast-4"
 BACKUP_BUCKET_NAME="$(aws ssm get-parameter --name "/backups/backups-bucket-name" | jq --raw-output ".Parameter.Value")"
-aws s3 cp --recursive deploy/*/writefreely/backups/*/home/write/backups "s3://${BACKUP_BUCKET_NAME}/write/"
+aws s3 cp --recursive deploy/writefreely/backups/*.tar "s3://${BACKUP_BUCKET_NAME}/writefreely/"
