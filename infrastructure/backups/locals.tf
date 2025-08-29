9c2a80cc908db6f109b2ed3269e022d77eb3b736
Nicholas Whittaker <nicholas@nicholas.cloud>
2025-08-29 23:31:20 +1000

    Remove Writefreely

--- infrastructure/backups/locals.tf
+++ infrastructure/backups/locals.tf
@@ -5,12 +5,5 @@ locals {
       schedule = "0 6 * * 6 Australia/Melbourne"
       pipeline = "backup-vault"
     }
-
-    writefreely = {
-      name = "Writefreely",
-      # Every 4th SAT https://github.com/floraison/fugit#the-modulo-extension
-      schedule = "0 6 * * 6%4 Australia/Melbourne"
-      pipeline = "backup-writefreely"
-    }
   }
 }
