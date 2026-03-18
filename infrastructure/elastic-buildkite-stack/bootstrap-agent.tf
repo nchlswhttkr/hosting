9f7617f1a42c3559c8cd2a86d4aa26c8660e2583
Nicholas Whittaker <nicholas@nicholas.cloud>
2026-03-18 18:38:55 +1100

    Remove workaround for Buildkite Tailscale access

--- infrastructure/elastic-buildkite-stack/bootstrap-agent.tf
+++ infrastructure/elastic-buildkite-stack/bootstrap-agent.tf
@@ -16,12 +16,6 @@ resource "aws_s3_object" "agent_bootstrap_script" {
         sudo dnf -y config-manager --add-repo https://rpm.releases.hashicorp.com/AmazonLinux/hashicorp.repo
         sudo dnf -y install terraform vault
 
-        # TODO: Remove this once stack supports AWS CLI >2.32.0
-        # https://github.com/buildkite/elastic-ci-stack-for-aws/blob/HEAD/packer/linux/base/scripts/versions.sh#L6
-        curl -sSfL "https://awscli.amazonaws.com/awscli-exe-linux-x86_64-2.33.3.zip" -o "awscliv2.zip"
-        unzip -qq awscliv2.zip
-        sudo ./aws/install --update
-
         TAILSCALE_CLIENT_ID="$(aws ssm get-parameter --name "${aws_ssm_parameter.tailscale_client_id.name}" --query "Parameter.Value" --output "text")"
         AWS_IDENTITY_TOKEN="$(aws sts get-web-identity-token --audience "api.tailscale.com/$TAILSCALE_CLIENT_ID" --signing-algorithm "ES384" --query "WebIdentityToken" --output "text")"
         sudo tailscale up --client-id "$TAILSCALE_CLIENT_ID" --id-token "$AWS_IDENTITY_TOKEN" --hostname "buildkite" --advertise-tags "tag:buildkite"
